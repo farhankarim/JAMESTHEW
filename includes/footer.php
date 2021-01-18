60333cf710f1373ace48cd998ae8d0ed4c09a695
farhankarim <farhankarim507@gmail.com>
2021-01-18 11:52:50 -0800

    project files

---
+++ includes/footer.php
@@ -0,0 +1,36 @@
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website, year | <a href="https://html5-templates.com/" target="_blank" rel="nofollow">HTML5 Templates</a></p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+
+<php?
+
+    include("includes/database.php");
+
+    class Car extends Vehicle{
+
+    }
+
+    $audi = new Car();
+    echo $audi;
+?>
+    </script>
---
+++ includes/main.php
@@ -0,0 +1,10 @@
+<?php
+
+include("includes/head.php");
+include("includes/lnav.php");
+
+include("includes/main.php")
+
+include("includes/rnav.php");
+include("includes/footer.php");
+?>
